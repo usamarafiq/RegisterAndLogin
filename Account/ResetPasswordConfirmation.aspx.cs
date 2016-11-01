65e8d674318946a6711ae6df0088df1d1526500e
USAMA\usamarana <usama.rao@hotmail.com>
2016-11-01 14:42:25 +0500

    initil commit

---
+++ Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace RegisterAndLogin.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RegisterAndLogin.Global" Language="C#" %>
